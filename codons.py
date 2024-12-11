5e6861e61604ec5b17e30e4e22e795d606435144
Omershani1 <shani.omer1@gmail.com>
2024-12-11 19:24:49 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,12 +1,12 @@
 def create_codon_dict(file_path):
-file = open(file_path)
-rows = file.readlines()
-file.close()
-d = {}
-for row in rows:
-    cells = row.strip().split('\t')
-    codon = cells[0]
-    amino_acid = cells[2]
-    d[codon] = amino_acid
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    d = {}
+    for row in rows:
+     cells = row.strip().split('\t')
+     codon = cells[0]
+     amino_acid = cells[2]
+     d[codon] = amino_acid
 return d
 
